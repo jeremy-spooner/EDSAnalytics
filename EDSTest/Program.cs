28237c713525480f4a537f676154237c0b6005f5
Jeremy Spooner <jspooner@osisoft.com>
2020-04-20 11:44:12 -0700

    removed more packages

--- EDSTest/Program.cs
+++ EDSTest/Program.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
